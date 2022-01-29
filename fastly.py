55091b91669a439a27412d5ceae0daa70d069f13
hks007 <66154793+hks007@users.noreply.github.com>
2022-01-29 11:47:30 +0530

    Update fastly.py

--- fastly.py
+++ fastly.py
@@ -37,7 +37,7 @@ async def fastly_bot(event):
         txt = out["ParsedResults"][0]["ParsedText"]
     except (KeyError, IndexError):
         return
-    txt = txt.replace("By@FastlyWriteBot", "").replace("\n", "").replace("\r", "")
+    txt = txt.replace("By@FastlyWriteBot", "").replace("By@Fast1yWriteBot", "").replace("\n", "").replace("\r", "")
     if txt:
         try:
             await event.reply(txt)
